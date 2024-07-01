455db8efae6e2c2a1f1e7296525ef663d2f79bd9
alisonobregon <alisonalejandra_19@hotmail.es>
2024-07-01 20:51:34 +0200

    s

--- source/main.c
+++ source/main.c
@@ -10,15 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <errno.h>
+#include "so_long.h"
 
 
 int main (int argc, char *argv[])
 {
-	
+	t_complete game;
 	if (argc != 2)
 		perror ("Numero de argumenntos no valido");
-
-
-
+	ft_memset(&game, 0, sizeof(t_complete));
+	read_map(&game, argv);
 }
