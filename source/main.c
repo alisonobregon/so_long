c864be86d81c18207379affc3c5be9272ea873f7
Alison Alejandra Obregon Jimenez <aliobreg@c1r5s5.42madrid.com>
2024-07-02 18:37:39 +0200

    s

--- source/main.c
+++ source/main.c
@@ -6,18 +6,21 @@
 /*   By: aliobreg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 16:59:55 by aliobreg          #+#    #+#             */
-/*   Updated: 2024/06/26 18:35:20 by aliobreg         ###   ########.fr       */
+/*   Updated: 2024/07/02 16:32:56 by aliobreg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-
 int main (int argc, char *argv[])
 {
 	t_complete game;
-	if (argc != 2)
-		perror ("Numero de argumenntos no valido");
+	if (argc != 2 && verificity_ber(argv))
+		perror ("Numero de argumentos no valido");
+	if (argc == 2 && !(verificity_ber(argv)))
+		perror ("Archivo no valido");
+	//if (argc && verificity_ber(argv)) algo que entre a la funcion
+	
 	ft_memset(&game, 0, sizeof(t_complete));
 	read_map(&game, argv);
 }
--- source/map.c
+++ source/map.c
@@ -6,17 +6,31 @@
 /*   By: aliobreg <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 18:35:41 by aliobreg          #+#    #+#             */
-/*   Updated: 2024/06/26 18:49:59 by aliobreg         ###   ########.fr       */
+/*   Updated: 2024/07/02 16:32:14 by aliobreg         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
 
 void read_map(t_complete game, char *argv)
 {
 	game -> fd = open(argv , O_RDONLY);
 	if( game -> fd < 0)
 		perror("Error reconociendo el archivo");
+
 }
+
+bool verificity_ber(char *argv)
+{
+	char	*save;
+
+	save = ft_strrchr(argv, '.');
+	if (save)
+		return (ft_strncmp(save, "ber") == 0);
+	return (0);
+}
+
+char *verificity_double(const )
 
 
